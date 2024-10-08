3760f7fb18fa62db5f8a7f2d9fab7e2736a8b7c9
misha <misha.kovalew2002@gmail.com>
2024-10-08 21:07:03 +0300

    Used labeled statement with declaration function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 "use strict";
 
-top: for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (j === 2 && i === 2)
-            break top;
-        console.log(i + " : " + j);
-    }
+L: function f() {
+    console.log("Hello");
+
+    console.log("sgwg");
 }
+
+f();
+
+console.log("World!")
