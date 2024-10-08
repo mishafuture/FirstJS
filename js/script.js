a3edef487b9f801f52dd7a7017921da23361581c
misha <misha.kovalew2002@gmail.com>
2024-10-08 20:47:39 +0300

    Used labeled statement

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 "use strict";
 
-const num = 37;
-
-(num % 2 === 0) ? console.log(`Number ${num} is even`) : console.log(`Number ${num} is odd`);
-
-switch (num) {
-    case 0:
-        console.log(`Number is 0`);
-        break;
-
-    case 1:
-        console.log(`Number is 1`);
-        break;
-
-    default:
-        console.log('Exit');
-        break;
+top: for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= 5; j++) {
+        if (j === 2 && i === 2)
+            break top;
+        console.log(i + " : " + j);
+    }
 }
-
-// console.log(personalMovieDB);
