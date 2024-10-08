e447ede2dafef34cef843a44e975cfa0c8d6c3e5
misha <misha.kovalew2002@gmail.com>
2024-10-08 14:24:19 +0300

    Created object PersonalMovieDB

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 let numberOfFilms = prompt('How many films have you watched?', '');
 
 // console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+console.log(personalMovieDB);
