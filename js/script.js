c86838dcf672d1649fd7d5f23fb52f325a3c45af
misha <misha.kovalew2002@gmail.com>
2024-10-09 12:47:40 +0300

    Added new check for the last film's name user watched, added if for number films

--- js/script.js
+++ js/script.js
@@ -15,15 +15,30 @@ let i = 0;
 
 while (i < countFilmsRequest) {
     let lastFilm = '';
+    let rate = '';
 
     do {
         lastFilm = prompt('What is the name of the last film you watched?', 'Back to the Future');
+        rate = prompt('How much did they rate the film??', '1');
     }
-    while (lastFilm === '' || lastFilm.length > 50);
+    while (lastFilm === null ||
+        lastFilm === '' ||
+        lastFilm.length > 50 ||
+        rate === null ||
+        rate === '');
 
-    personalMovieDB.movie[lastFilm] = prompt('How much did they rate the film??', '1');
+    personalMovieDB.movie[lastFilm] = rate;
 
     i++;
 }
+
+if (numberOfFilms < 10)
+    console.log('Few films were watched');
+else if (numberOfFilms < 30)
+    console.log('You are an ordinary viewer');
+else if (numberOfFilms >= 30)
+    console.log('You are a movie buff');
+else
+    console.log('Error');
 
 console.log(personalMovieDB);
