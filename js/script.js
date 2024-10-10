7066ff74f5d3dc9da8149436b7e70f2b761b8506
misha <misha.kovalew2002@gmail.com>
2024-10-10 16:02:24 +0300

    added all tasks for string

--- js/script.js
+++ js/script.js
@@ -6,4 +6,16 @@ function ucFirst(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
 
-console.log(ucFirst(str));
+function checkSpam(str) {
+    let convertedStr = str.toLowerCase();
+
+    return convertedStr.includes('viagra') ||
+        convertedStr.includes('xxx');
+}
+
+function truncate(str, maxLength) {
+    return str.length > maxLength ? str.slice(0, maxLength-1) + '…' : str;
+}
+
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+console.log(truncate("Всем привет!", 20));
