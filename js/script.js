57e999a073b97ba8a5d3042a1a25ceb792ced1f8
misha <misha.kovalew2002@gmail.com>
2024-10-10 21:26:44 +0300

    solved task read number from user

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
 'use strict';
 
-let str = "hello world";
+function readNumber() {
+    let num;
 
-function ucFirst(str) {
-    return str[0].toUpperCase() + str.slice(1);
+    do {
+        num = prompt("Enter your number", "");
+    }
+    while (!isFinite(num));
+
+    return (num === null || num === '') ? null : parseInt(num);
 }
 
-function checkSpam(str) {
-    let convertedStr = str.toLowerCase();
-
-    return convertedStr.includes('viagra') ||
-        convertedStr.includes('xxx');
-}
-
-function truncate(str, maxLength) {
-    return str.length > maxLength ? str.slice(0, maxLength-1) + '…' : str;
-}
-
-console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
-console.log(truncate("Всем привет!", 20));
+alert(readNumber());
