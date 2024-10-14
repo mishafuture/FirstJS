4e5043dc071617996335e5c4c13927e018bbc632
misha <misha.kovalew2002@gmail.com>
2024-10-14 13:38:11 +0300

    added for in loop for object and done object destructuring

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
 'use strict';
 
-function loadScripts(src, callback) {
-    let script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = src;
-    script.onload = () => callback(script);
-    document.head.append(script);
+let obj = {
+    name: 'Misha',
+    age: 21,
+    password: '123456',
+    favoriteColors: ['red', 'green', 'blue'],
+    favoriteMovies: {
+        StarWars: 10,
+        HarryPotter: 9
+    },
+    makeTest: function() {
+        console.log('Make test');
+    }
+};
+
+/*
+console.log(obj);
+
+delete obj.password;
+
+console.log(obj);
+
+for (let key in obj) {
+    if (Object.is(typeof obj[key], "object")) {
+        console.log(`Complex property ${key} includes`);
+        for (let subKey in obj[key]) {
+            console.log(`\tProperty: ${subKey} is ${obj[key][subKey]}`);
+        }
+    }
+    else
+        console.log(`Property: ${key} is ${obj[key]}`);
 }
 
-loadScripts('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js',
-    script => {
-    alert(`Script ${script} is already loaded.`);
-    alert( _ );
-});
+console.log(Object.keys(obj).length);
+obj.makeTest();*/
+
+console.log(obj["favoriteMovies"]["StarWars"]);
+
+const {StarWars, HarryPotter} = obj["favoriteMovies"];
+
+console.log(StarWars);
