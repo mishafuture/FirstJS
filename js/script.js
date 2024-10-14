e01d2709f3996ffc50144ccaa802aa319e4c0fa2
misha <misha.kovalew2002@gmail.com>
2024-10-14 14:20:14 +0300

    added some practice with object destructuring

--- js/script.js
+++ js/script.js
@@ -1,42 +1,14 @@
 'use strict';
 
-let obj = {
-    name: 'Misha',
-    age: 21,
-    password: '123456',
-    favoriteColors: ['red', 'green', 'blue'],
-    favoriteMovies: {
-        StarWars: 10,
-        HarryPotter: 9
-    },
-    makeTest: function() {
-        console.log('Make test');
+let options = {
+    title: "menu",
+    size: {
+        width: 100,
+        height: 200
     }
 };
 
-/*
-console.log(obj);
+const {title, size: {width, height}} = options;
 
-delete obj.password;
-
-console.log(obj);
-
-for (let key in obj) {
-    if (Object.is(typeof obj[key], "object")) {
-        console.log(`Complex property ${key} includes`);
-        for (let subKey in obj[key]) {
-            console.log(`\tProperty: ${subKey} is ${obj[key][subKey]}`);
-        }
-    }
-    else
-        console.log(`Property: ${key} is ${obj[key]}`);
-}
-
-console.log(Object.keys(obj).length);
-obj.makeTest();*/
-
-console.log(obj["favoriteMovies"]["StarWars"]);
-
-const {StarWars, HarryPotter} = obj["favoriteMovies"];
-
-console.log(StarWars);
+console.log(title);
+console.log(width);
