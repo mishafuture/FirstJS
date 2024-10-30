8a3cc8b75fcf5406100d26f778998e2f5bd4cd07
misha <misha.kovalew2002@gmail.com>
2024-10-30 21:05:14 +0200

    improved site

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 Реализовать только при помощи JS
 
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+Отсортировать их по алфавиту
 
 5) Добавить нумерацию выведенных фильмов */
 
@@ -24,21 +24,69 @@ const movieDB = {
     ]
 };
 
-document.querySelectorAll('.promo__adv img').forEach((img) => {
-    img.remove();
-})
-document.querySelector('.promo__genre').innerText = 'Драма';
-document.querySelector('.promo__bg').style.backgroundImage = 'url("img/bg.jpg")';
-const filmList = document.querySelector('.promo__interactive-list');
+document.addEventListener('DOMContentLoaded', () => {
+    const inputMovie = document.getElementsByClassName('adding__input')[0];
+    const btnSave = document.getElementsByTagName('button')[0];
+    const filmsList = document.querySelector('.promo__interactive-list');
+    const isFavorite = document.querySelector('input[type="checkbox"]');
 
-filmList.innerHTML = '';
+    document.querySelectorAll('.promo__adv img').forEach((img) => {
+        img.remove();
+    })
+    document.querySelector('.promo__genre').innerText = 'Драма';
+    document.querySelector('.promo__bg').style.backgroundImage = 'url("img/bg.jpg")';
 
-movieDB.movies.sort().forEach((movie, i) => {
-    filmList.innerHTML += `
-        <li class="promo__interactive-item">${++i}. ${movie}</li>
-            <div class="delete"></div>
-        </li>
-    `;
+    const createList = (path, database) => {
+        path.innerHTML = '';
+
+        database.forEach((movie, i) => {
+            path.innerHTML += `
+            <li class="promo__interactive-item">${++i}. ${movie}
+                <div class="delete"></div>
+            </li>
+            `;
+        });
+
+        filmsList.querySelectorAll('.delete')
+            .forEach((movie, i) => {
+                movie.addEventListener('click', function () {
+                    movie.parentElement.remove();
+                    movieDB.movies.splice(i, 1);
+                    createList(filmsList, movieDB.movies);
+                })
+        });
+    }
+
+    movieDB.movies.sort();
+    createList(filmsList, movieDB.movies);
+
+    btnSave.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        let movie = inputMovie.value;
+
+        if (movie) {
+            if (movie.length > 21)
+                movie = movie.substring(0, 20) + '...';
+
+            movieDB.movies.push(movie);
+            movieDB.movies.sort();
+
+            if (isFavorite.checked) {
+                console.log("Adding favorite movie " + movie);
+                isFavorite.checked = false;
+            }
+
+
+            createList(filmsList, movieDB.movies);
+
+            inputMovie.value = '';
+        }
+
+
+    });
 });
+
+
 
 
