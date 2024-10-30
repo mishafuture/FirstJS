6aa4127522006c998b595028e0bcad3ae2c71110
misha <misha.kovalew2002@gmail.com>
2024-10-30 16:52:49 +0200

    added recursion example and removed trashed files

--- js/script.js
+++ js/script.js
@@ -1,44 +1,90 @@
-/* Задания на урок:
+"use strict";
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+/*
+function pow(number, degree) {
+    return (degree === 1 ? number : number * pow(number, degree - 1));
+}
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+console.log(pow(2, 2));
+console.log(pow(2, 4));
+console.log(pow(2, 5));
+console.log(pow(2, 3));*/
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+let students = {
+    js : [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+    html : {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ivan',
+            progress: 18
+        }],
 
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
 };
 
-document.querySelectorAll('.promo__adv img').forEach((img) => {
-    img.remove();
-})
-document.querySelector('.promo__genre').innerText = 'Драма';
-document.querySelector('.promo__bg').style.backgroundImage = 'url("img/bg.jpg")';
-const filmList = document.querySelector('.promo__interactive-list');
+function getTotalProgressByIteration(data) {
+    let count = 0;
+    let total = 0;
 
-filmList.innerHTML = '';
+    for (const course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            count += course.length;
 
-movieDB.movies.sort().forEach((movie, i) => {
-    filmList.innerHTML += `
-        <li class="promo__interactive-item">${++i}. ${movie}</li>
-            <div class="delete"></div>
-        </li>
-    `;
-});
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        }
+        else {
+            for (const subCourse of Object.values(course)) {
+                count += subCourse.length;
 
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
 
+    return total / count;
+}
+
+function getTotalProgressByRecursion(data) {
+    let total = 0;
+
+    if (Array.isArray(data)) {
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+
+        return [total, data.length];
+    }
+    else {
+        let total = [0, 0];
+
+        for (const subData of Object.values(data)) {
+            const subDataArray = getTotalProgressByRecursion(subData);
+            total[0] += subDataArray[0];
+            total[1] += subDataArray[1];
+        }
+
+        return total;
+    }
+}
+
+console.log(getTotalProgressByIteration(students));
+
+const result = getTotalProgressByRecursion(students);
+console.log(result[0] / result[1]);
