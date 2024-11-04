54804dd8291242437daefc99f9ace050389b0834
misha <misha.kovalew2002@gmail.com>
2024-11-04 16:16:57 +0200

    check defer and async

--- js/script.js
+++ js/script.js
@@ -1,40 +1,13 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
+console.log(document.querySelectorAll('p'))
 
-    let startTime;
-    let betw = {}
-    let endTime;
+function loadScripts(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+    document.body.append(script);
+}
 
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-
-        startTime = new Date().getTime();
-        betw.x = e.changedTouches[0].pageX;
-        betw.y = e.changedTouches[0].pageY;
-    }, false);
-
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-
-        endTime = new Date().getTime();
-        if (e.changedTouches[0].pageX === betw.x && e.changedTouches[0].pageY === betw.y &&
-            (endTime - startTime) > 800) {
-            console.log("It was long tap");
-        }
-        else
-            console.log("It was common tap");
-    }, false);
-
-/*    box.addEventListener('touchmove', (e) => {
-        console.log('move');
-        console.log(e.targetTouches[0].pageX);
-    });
-
-    box.addEventListener('touchend', () => {
-        console.log('end');
-    });*/
-});
+loadScripts("js/test.js");
+loadScripts("js/test1.js");
