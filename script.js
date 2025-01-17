5fefda5eb5e498c9bf283c9dedab1be5c37708d6
misha <misha.kovalew2002@gmail.com>
2025-01-17 16:04:26 +0200

    counter by iife

--- script.js
+++ script.js
@@ -1,15 +1,98 @@
-'use strict';
+"use strict";
 
-const boxesQuery = document.querySelectorAll('.box');
-const boxesGet = document.getElementsByClassName('box');
+const counter = (function () {
+    let count = 0;
 
-boxesQuery.forEach(box => {
-    if (box.matches('.this')) {
-        console.log(box);
+    return {
+        increment: function () {
+            count++;
+        },
+        decrement: function () {
+            count--;
+        },
+        getValue: function () {
+            return count;
+        }
     }
-});
+})();
 
-console.log(boxesQuery[0].closest('.container'));
+counter.increment();
+counter.increment();
+counter.increment();
+console.log(counter.getValue());
+counter.decrement();
+counter.decrement();
+console.log(counter.getValue());
 
-console.log(boxesQuery);
-console.log(boxesGet);
+const counter1 = {
+    count: 0,
+    increment: function () {
+        this.count += 1;
+    },
+    decrement: function () {
+        this.count -= 1;
+    },
+    getValue: function () {
+        return this.count;
+    }
+}
+
+/*class Menu {
+    constructor(elem) {
+        elem.onclick = this.onClick.bind(this);
+    }
+
+    getPersonInfo() {
+        const name = document.querySelector("#nameInput").value;
+        const age = document.querySelector("#ageInput").value;
+
+        return [name, age];
+    }
+
+    add() {
+        const newTr = document.createElement("tr");
+        const newTdName = document.createElement("td");
+        const newTdAge = document.createElement("td");
+
+        const [name, age] = this.getPersonInfo();
+
+        newTdName.textContent = name;
+        newTdAge.textContent = age;
+
+        newTr.append(newTdName);
+        newTr.append(newTdAge);
+        tableBody.append(newTr);
+    }
+
+    delete() {
+        const tableRows = tableBody.querySelectorAll("tr");
+        const [name, age] = this.getPersonInfo();
+
+        for (let i = 0; i < tableRows.length; i++) {
+            const cells = tableRows[i].querySelectorAll("td");
+
+            if (cells[0].textContent.trim() === name &&
+                cells[1].textContent.trim() === age) {
+                tableRows[i].remove();
+            }
+        }
+    }
+
+    search() {
+        alert("was found");
+    }
+
+    onClick() {
+        let action = event.target.dataset.action;
+
+        if (action) {
+            this[action]();
+        }
+    }
+}
+
+let menu = document.querySelector('#menu'),
+    tableBody = document.querySelector('tbody');
+
+new Menu(menu);*/
+
