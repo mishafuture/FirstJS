8c3aed903b53d60bdccf2f11913547cf82d82c88
misha <misha.kovalew2002@gmail.com>
2025-01-23 18:28:06 +0200

    function delay with promise

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-"use strict";
+'use strict';
 
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    calculateArea() {
-        return this.width * this.height;
-    }
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-class ColoredRectangleWithText extends Rectangle {
-    constructor(width, height, text, backgroundColor) {
-        super(width, height);
-        this.text = text;
-        this.backgroundColor = backgroundColor;
-    }
-
-    showMyProps() {
-        console.log(`My Props: text '${this.text}' and backgroundColor is ${this.backgroundColor} and has area ${this.calculateArea()}`);
-    }
-}
-
-const square = new Rectangle(10, 10);
-const longRectangle = new Rectangle(20, 100);
-
-console.log(square.calculateArea());
-console.log(longRectangle.calculateArea());
-
-const redRectangle = new ColoredRectangleWithText(20, 30, 'New Rectangle', 'red');
-
-redRectangle.showMyProps();
+delay(3000).then(() => alert('выполнилось через 3 секунды'));
