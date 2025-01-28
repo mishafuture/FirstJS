3491d0faa4a4522cf9d9fd72c988f92b7a71beec
misha <misha.kovalew2002@gmail.com>
2025-01-28 23:39:53 +0200

    realise small tasks with HMLHttpRequest object and Fetch API

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 'use strict';
 
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'data.txt', true);
+xhr.send(null);
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log(xhr.responseText);
+    }
 }
 
-delay(3000).then(() => alert('выполнилось через 3 секунды'));
+fetch('data.txt')
+    .then(res => res.text())
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message));
+
+fetch('‘https://api.example.com/data', {
+    method: 'POST',
+    body: JSON.stringify({key:`value`}),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
