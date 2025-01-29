dfd67ff62f8484a060e1a448f072f6ebc82e7adc
misha <misha.kovalew2002@gmail.com>
2025-01-29 23:46:55 +0200

    created function memoize (with 1 argument and some arguments)

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
 'use strict';
 
-function Example(name, text) {
-    this.name = name;
-    this.text = text;
-    this.getThis = () => this;
+/*
+function memoize(fn) {
+    const cache = new Map();
+
+    return function (param) {
+        if (cache.has(param)) {
+            console.log(`Parameter ${param} has already cashed`);
+
+            return cache.get(param);
+        }
+
+        console.log(`Parameter ${param} hasn't cashed`);
+        const result = fn(param);
+        cache.set(param, result);
+
+        return result;
+    }
 }
-//Example.prototype = 42;
 
-const example = new Example('test', 'some text');
-const example1 = new Example('test1', 'some text1');
-console.log(example.getThis());
+const memoizeOne = memoize((param) =>
+    param.toUpperCase());
 
-Example.prototype.getName = function () {
-    return this.name;
+console.log(memoizeOne('one'));
+console.log(memoizeOne('one'));
+console.log(memoizeOne('three'));*/
+
+function memoize(fn) {
+    const cache = new Map();
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            console.log(`Parameters ${args} has already cashed`);
+
+            return cache.get(key);
+        }
+
+        console.log(`Parameters ${args} hasn't cashed`);
+        const result = fn(args);
+        cache.set(key, result);
+
+        return result;
+    }
 }
 
-console.log(example1.getName());
-console.log(example.getName());
+const makeFullName = (fName, lName) => `${fName} ${lName}`
+const reduceAdd = (numbers, startValue = 0) => numbers.reduce((total, cur) => total + cur, startValue)
+
+const memoizedFullName = memoize(makeFullName)
+const memoizeReduceAdd = memoize(reduceAdd)
+
+memoizedFullName('Marko', 'Polo')
+memoizedFullName('Marko', 'Polo') // не выполнится
+
+memoizeReduceAdd([1,2,3,4],5)
+memoizeReduceAdd([1,2,3,4],5) // не выполнится
