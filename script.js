3d95726c55f6855194fe0111ddac6e888fa62585
misha <misha.kovalew2002@gmail.com>
2025-01-30 00:39:42 +0200

    realization of event-delegation

--- script.js
+++ script.js
@@ -1,59 +1,29 @@
 'use strict';
 
-/*
-function memoize(fn) {
-    const cache = new Map();
+class Menu {
+    constructor(elem) {
+        elem.onclick = this.onClick.bind(this);
+    }
 
-    return function (param) {
-        if (cache.has(param)) {
-            console.log(`Parameter ${param} has already cashed`);
+    save() {
+        alert('saving...');
+    }
+    delete() {
+        alert('deleting...');
+    }
+    load() {
+        alert('loading...');
+    }
 
-            return cache.get(param);
+    onClick (event) {
+        const action = event.target.dataset.action;
+
+        if (action) {
+            this[action]();
         }
-
-        console.log(`Parameter ${param} hasn't cashed`);
-        const result = fn(param);
-        cache.set(param, result);
-
-        return result;
     }
 }
 
-const memoizeOne = memoize((param) =>
-    param.toUpperCase());
+const menu = document.querySelector('.menu');
 
-console.log(memoizeOne('one'));
-console.log(memoizeOne('one'));
-console.log(memoizeOne('three'));*/
-
-function memoize(fn) {
-    const cache = new Map();
-
-    return function (...args) {
-        const key = JSON.stringify(args);
-
-        if (cache.has(key)) {
-            console.log(`Parameters ${args} has already cashed`);
-
-            return cache.get(key);
-        }
-
-        console.log(`Parameters ${args} hasn't cashed`);
-        const result = fn(args);
-        cache.set(key, result);
-
-        return result;
-    }
-}
-
-const makeFullName = (fName, lName) => `${fName} ${lName}`
-const reduceAdd = (numbers, startValue = 0) => numbers.reduce((total, cur) => total + cur, startValue)
-
-const memoizedFullName = memoize(makeFullName)
-const memoizeReduceAdd = memoize(reduceAdd)
-
-memoizedFullName('Marko', 'Polo')
-memoizedFullName('Marko', 'Polo') // не выполнится
-
-memoizeReduceAdd([1,2,3,4],5)
-memoizeReduceAdd([1,2,3,4],5) // не выполнится
+new Menu(menu);
