5d6813b881739b113692ae35d5ee2a0fa897b429
misha <misha.kovalew2002@gmail.com>
2025-02-05 21:45:32 +0200

    test weakMap and weakSet

--- script.js
+++ script.js
@@ -1,29 +1,75 @@
 'use strict';
 
-const tGrid = document.querySelector('#grid');
+/*
+const cache = new WeakMap();
 
-tGrid.addEventListener('click', (event) => {
-    const target = event.target;
+function cacheUser(user) {
+    if (!cache.has(user)) {
+        console.log(user.name + ' has been cached');
+        cache.set(user, Date.now());
+    }
+    return cache.get(user);
+}
 
-    if (target.tagName.toLowerCase() !== 'th') return;
+let anna = {name: 'anna'};
+const ben = {name: 'ben'};
+cacheUser(anna);
+cacheUser(anna);
+cacheUser(ben);
 
-    const thIndex = target.cellIndex;
-    const type = target.dataset.type;
-    const tbody = tGrid.querySelector('tbody');
-    const rows = Array.from(tbody.rows);
+anna = null;
 
-    const sortedRows = rows.sort((rowA, rowB) => {
-        const a = rowA.cells[thIndex].innerText.trim();
-        const b = rowB.cells[thIndex].innerText.trim();
+console.log(cache.has(anna));
+console.log(cache.has(ben));*/
 
-        if (type === 'number') {
-            return a - b;
+/*
+let messages = [
+    {text: 'Hello', from: 'John'},
+    {text: 'World', from: 'Anna'},
+    {text: 'Hi', from: 'Misha'},
+];
+
+let readMessages = new WeakSet();
+readMessages.add(messages[0]);
+readMessages.add(messages[2]);
+
+let sym = Symbol(2);
+
+readMessages.add(sym);
+
+readMessages.add(messages[0]);
+
+messages.shift();
+
+console.log(readMessages.has(messages[0]));
+console.log(readMessages.has(messages[1]));
+console.log(readMessages.has(messages[2]));
+*/
+
+// Выполняем `fn` для всего, что хранится внутри объекта.
+function execRecursively(fn, subject, _refs = new WeakSet()) {
+    // Избегаем бесконечно рекурсии
+    if (_refs.has(subject)) {
+        return;
+    }
+
+    fn(subject);
+    if (typeof subject === "object" && subject) {
+        _refs.add(subject);
+        for (const key in subject) {
+            execRecursively(fn, subject[key], _refs);
         }
-        else if (type === 'string') {
-            return (a.toLowerCase()).localeCompare(b.toUpperCase());
-        }
-        return new Error('Inappropriate data type');
-    })
+        _refs.delete(subject);
+    }
+}
 
-    tbody.append(...sortedRows);
-})
+const foo = {
+    foo: "Foo",
+    bar: {
+        bar: "Bar",
+    },
+};
+
+foo.bar.baz = foo; // Циклическая ссылка!
+execRecursively((obj) => console.log(obj), foo);
+
