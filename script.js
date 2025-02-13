9a922d4e45f886fdca7371f4d2449e6e5235b7d0
misha <misha.kovalew2002@gmail.com>
2025-02-13 15:26:51 +0200

    practice with rest operator

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
 'use strict';
 
-const inputArray = [7, 2, 5, 1, 3, 4, 8, 1];
-
-function sort (array) {
-    const evenArray = [],
-        oddArray = [];
-
-    array.forEach((item) => {
-        if (item % 2 === 0)
-            evenArray.push(item);
-        else
-            oddArray.push(item);
-    });
-
-    function bubbleSort (array, compare) {
-        let length = array.length - 1,
-            swapped;
-
-        do {
-            swapped = false;
-            for (let j = 0; j < length; j++) {
-                if (compare(array[j], array[j + 1])) {
-                    let temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            length--;
-        } while(swapped);
-    }
-
-    bubbleSort(oddArray, (a, b) => a < b);
-    bubbleSort(evenArray, (a, b) => a > b);
-
-    return [...oddArray, ...evenArray];
+const log = function (a, b, ...rest) {
+    console.log(a, b, rest);
 }
 
-const result = sort(inputArray);
-console.log(result);
+log('test', 'test1', 'basic', 'info', 'user');
+
+function calcOrDouble(num, basis = 2) {
+    console.log(num * basis);
+}
+
+calcOrDouble(4, 5);
+calcOrDouble(5);
 
