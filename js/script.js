be6fd33509977ea96aeee3d0427038c8c0c2aaff
misha <misha.kovalew2002@gmail.com>
2025-02-17 19:53:47 +0200

    some practice with promise

--- js/script.js
+++ js/script.js
@@ -1,46 +1,75 @@
 'use strict';
 
-let inputUAH = document.querySelector('#UAH'),
-    inputUSD = document.querySelector('#USD');
+/*console.log("Request to database...");
 
-inputUAH.addEventListener('input', (e) => {
-   const request = new XMLHttpRequest();
+const req = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      console.log("Preparing data...");
 
-   request.open("GET", "js/current.json");
-   request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-   request.send();
-
-   /*request.addEventListener("readystatechange", (e) => {
-      if (request.readyState === 4 && request.status === 200) {
-         const data = JSON.parse(request.response);
-         inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
-      } else {
-         inputUSD.value = 'Error';
+      const product = {
+         name: 'Grape',
+         price: 100,
       }
-   });*/
 
-/*   request.addEventListener('load', () => {
-      if (request.status === 200) {
-         const data = JSON.parse(request.response);
-         inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
-      } else {
-         inputUSD.value = 'Error';
-         request.abort();
-      }
-   });*/
-
-   request.onreadystatechange = function () {
-      if (request.readyState === 4) {
-         if (request.status === 200) {
-            const data = JSON.parse(request.response);
-            inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
-         } else {
-            inputUSD.value = 'Error';
-            request.abort();
-         }
-      } else {
-         console.log(request.readyState)
-         inputUSD.value = request.status;
-      }
-   }
+      resolve(product);
+   }, 2000);
 });
+
+req.then(product => {
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         product.status = 'ordered';
+         resolve(product);
+      }, 2000)
+   });
+}).then(data => {
+   data.modify = true;
+   return data;
+}).then(data => {
+   console.log(data);
+}).catch(() => {
+   console.error("Error occurred");
+}).finally(() => {
+   console.log("Database finished...");
+});*/
+
+/*
+const test = time =>
+    new Promise((resolve, reject) => {
+      setTimeout(resolve, time);
+});
+
+test(1000).then(() => {
+   console.log('1000 ms have passed!');
+});
+
+test(2000).then(() => {
+   console.log('2000 ms have passed!');
+});*/
+
+let resultA, resultB, resultC;
+function addAsync(num1, num2) {
+// ES6 подтягивает API, который возвращает промис.
+    return fetch(`http://www.example.com?num1=${num1}&num2=${num2}`)
+        .then(x => x.json());
+}
+
+addAsync(1, 2)
+    .then(success => {
+        resultA = success;
+        return resultA;
+    })
+    .then(success => addAsync(success, 3))
+    .then(success => {
+        resultB = success;
+        return resultB;
+    })
+    .then(success => addAsync(success, 4))
+    .then(success => {
+        resultC = success;
+        return resultC;
+    })
+    .then(success => {
+        console.log('total: ' + success)
+        console.log(resultA, resultB, resultC)
+    });
