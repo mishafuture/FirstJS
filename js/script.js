2ccf96cfa59d3847a536bd51837c79ae478a435e
misha <misha.kovalew2002@gmail.com>
2025-02-20 20:54:51 +0200

    js parser that looks for header tags

--- js/script.js
+++ js/script.js
@@ -1,75 +1,32 @@
 'use strict';
 
-/*console.log("Request to database...");
+window.addEventListener('DOMContentLoaded', () => {
+   const body = document.querySelector('body');
+   const textNode = [];
 
-const req = new Promise((resolve, reject) => {
-   setTimeout(() => {
-      console.log("Preparing data...");
+   function recursionNode(element) {
+        element.childNodes.forEach(node => {
+            if (node.nodeName.match(/^H\d/)) {
+                const obj = {
+                    header: node.nodeName,
+                    content: node.textContent.trim(),
+                }
+                textNode.push(obj);
+            } else {
+                recursionNode(node);
+            }
+        });
+   }
 
-      const product = {
-         name: 'Grape',
-         price: 100,
-      }
+   recursionNode(body);
 
-      resolve(product);
-   }, 2000);
-});
-
-req.then(product => {
-   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-         product.status = 'ordered';
-         resolve(product);
-      }, 2000)
-   });
-}).then(data => {
-   data.modify = true;
-   return data;
-}).then(data => {
-   console.log(data);
-}).catch(() => {
-   console.error("Error occurred");
-}).finally(() => {
-   console.log("Database finished...");
-});*/
-
-/*
-const test = time =>
-    new Promise((resolve, reject) => {
-      setTimeout(resolve, time);
-});
-
-test(1000).then(() => {
-   console.log('1000 ms have passed!');
-});
-
-test(2000).then(() => {
-   console.log('2000 ms have passed!');
-});*/
-
-let resultA, resultB, resultC;
-function addAsync(num1, num2) {
-// ES6 подтягивает API, который возвращает промис.
-    return fetch(`http://www.example.com?num1=${num1}&num2=${num2}`)
-        .then(x => x.json());
-}
-
-addAsync(1, 2)
-    .then(success => {
-        resultA = success;
-        return resultA;
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(textNode),
     })
-    .then(success => addAsync(success, 3))
-    .then(success => {
-        resultB = success;
-        return resultB;
-    })
-    .then(success => addAsync(success, 4))
-    .then(success => {
-        resultC = success;
-        return resultC;
-    })
-    .then(success => {
-        console.log('total: ' + success)
-        console.log(resultA, resultB, resultC)
-    });
+        .then(res => res.json())
+        .then(data => {console.log(data)});
+});
