6039f7e88d58cb4463a42dc8d7cff7f911f13bcc
misha <misha.kovalew2002@gmail.com>
2025-02-27 17:22:54 +0200

    remove cookie consent and test reg exp

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+const asw = prompt('What is your name?', 'Misha');
+const reg = /i/ig;
+
+console.log(asw.match(reg));*/
+
+/*const password = prompt('Password');
+console.log(password.replace(/./g, '*'));*/
+
+const asw = 'My name is R2D2';
+const reg = /\w/g;
+console.log(asw.match(reg));
