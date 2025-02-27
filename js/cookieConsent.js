869181877f4fbd629b93af8fd338f0c2d9cd4612
misha <misha.kovalew2002@gmail.com>
2025-02-27 15:35:37 +0200

    remove class from DOMContentLoaded

--- js/cookieConsent.js
+++ js/cookieConsent.js
@@ -1,7 +1,7 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-    /*const cookieStorage = {
+/*window.addEventListener('DOMContentLoaded', () => {
+    const cookieStorage = {
         getItem: (key) => {
             const cookies = document.cookie.split(';')
                 .map(item => item.split('='))
@@ -41,75 +41,77 @@ window.addEventListener('DOMContentLoaded', () => {
     btnCancel.addEventListener('click', () => {
         toggleStorage(false);
         popup.classList.remove('popup_active');
-    })*/
+    })
+});*/
 
-    class cookieConsent {
-        constructor({popup, btnConfirm, btnCancel, activeClass = ''} = {}) {
-            this.popup = document.querySelector(popup);
-            this.btnConfirm = document.querySelector(btnConfirm);
-            this.btnCancel = document.querySelector(btnCancel);
-            this.activeClass = activeClass;
-            this.consentPropertyType = 'site_consent';
-        }
+class cookieConsent {
+    constructor({popup, btnConfirm, btnCancel, activeClass = ''} = {}) {
+        this.popup = document.querySelector(popup);
+        this.btnConfirm = document.querySelector(btnConfirm);
+        this.btnCancel = document.querySelector(btnCancel);
+        this.activeClass = activeClass;
+        this.consentPropertyType = 'site_consent';
+    }
 
-        getItem = (key) => {
-            const cookies = document.cookie.split(';')
-                .map(item => item.split('='))
-                .reduce((acc, [key, value]) => ({...acc,
-                    [key.trim()]: value}), {});
+    getItem = (key) => {
+        const cookies = document.cookie.split(';')
+            .map(item => item.split('='))
+            .reduce((acc, [key, value]) => ({...acc,
+                [key.trim()]: value}), {});
 
-            return decodeURIComponent(cookies[key]);
-        }
+        return decodeURIComponent(cookies[key]);
+    }
 
-        setItem = (key, value) => {
-            document.cookie = `${key}=${encodeURIComponent(value)};expires=Fri, 28 Feb 2025 12:00:00 GMT`;
-        }
+    setItem = (key, value) => {
+        document.cookie = `${key}=${encodeURIComponent(value)};expires=Fri, 28 Feb 2025 12:00:00 GMT`;
+    }
 
-        hasConsented = () => this.getItem(this.consentPropertyType) === "true";
+    hasConsented = () => this.getItem(this.consentPropertyType) === "true";
 
-        changeStatus = (prop) => {
-            this.setItem(this.consentPropertyType, prop);
+    changeStatus = (prop) => {
+        this.setItem(this.consentPropertyType, prop);
 
-            if (this.hasConsented()) {
-                // Выполнить
-                myScripts();
-            }
-        }
-
-        bindTriggers = (btns) => {
-            btns.forEach((btn) => {
-                let status = btn === this.btnConfirm;
-
-                btn.addEventListener('click', () => {
-                    this.changeStatus(status);
-                    this.popup.classList.remove(this.activeClass);
-                })
-            })
-        }
-
-        init = () => {
-            try {
-                if (this.hasConsented()) {
-                    myScripts();
-                } else {
-                    this.popup.classList.add(this.activeClass);
-                }
-
-                this.bindTriggers([this.btnConfirm, this.btnCancel]);
-            } catch (e) {
-                console.error('Not all data transferred');
-            }
+        if (this.hasConsented()) {
+            // Выполнить
+            myScripts();
         }
     }
 
+    bindTriggers = (btns) => {
+        btns.forEach((btn) => {
+            let status = btn === this.btnConfirm;
+
+            btn.addEventListener('click', () => {
+                this.changeStatus(status);
+                this.popup.classList.remove(this.activeClass);
+            })
+        })
+    }
+
+    init = () => {
+        try {
+            if (this.hasConsented()) {
+                myScripts();
+            } else {
+                this.popup.classList.add(this.activeClass);
+            }
+
+            this.bindTriggers([this.btnConfirm, this.btnCancel]);
+        } catch (e) {
+            console.error('Not all data transferred');
+        }
+    }
+}
+
+function myScripts() {
+    console.log('Loading...')
+}
+
+window.addEventListener('DOMContentLoaded', () => {
     new cookieConsent({
         activeClass: 'popup_active',
         popup: '.popup',
         btnConfirm: '[data-confirm]',
         btnCancel: '[data-cancel]',
     }).init();
-
-    function myScripts() {
-        console.log('Loading...')
-    }
 });
