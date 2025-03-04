aeab9a63fcb3d61ff51de556f289fe6666403fc5
misha <misha.kovalew2002@gmail.com>
2025-03-04 16:10:34 +0200

    get and set reviews

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
 'use strict';
-
 /*
-const asw = prompt('What is your name?', 'Misha');
-const reg = /i/ig;
 
-console.log(asw.match(reg));*/
+const person = {
+    name: 'John',
+    age: 25,
 
-/*const password = prompt('Password');
-console.log(password.replace(/./g, '*'));*/
+    get userAge() {
+        return this.age;
+    },
 
-const asw = 'mis_ko@g.co';
-const emailCheck = /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
-console.log(emailCheck.test(asw));
+    set userAge(age) {
+        if (age >= 0) {
+            this.age = age;
+        }
+    }
+}
+
+console.log(person.userAge = -5);
+console.log(person.userAge);*/
+
+const person = {
+    get name() {
+        return this._name;
+    },
+
+    set name(name) {
+        if (name.length < 3) {
+            console.error('Name should be at least 3 characters long.');
+            return;
+        }
+        this._name = name;
+    }
+}
+
+person.name = 'Ra';
+console.log(person.name);
+
+person.name = 'Misha';
+console.log(person);
