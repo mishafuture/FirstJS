daaad8e54f8e3b439190055036cf8562ae5ef231
misha <misha.kovalew2002@gmail.com>
2025-03-10 12:53:58 +0200

    add API for automatic polyfill generation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// require('es6-promise').polyfill();
+// import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal, {openModal} from './modules/modal';
 import timer from './modules/timer';
@@ -7,11 +10,13 @@ import forms from './modules/forms';
 import slider from './modules/slider';
 
 window.addEventListener('DOMContentLoaded', () => {
+   console.log("HERE");
    const modalTimedId = setTimeout(() => openModal('.modal', modalTimedId), 300000);
 
    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
    modal('[data-modal]', '.modal', modalTimedId);
    timer('.timer', '2025-05-08');
+   console.log('Timer pass');
    cards();
    calculator();
    forms('form', modalTimedId);
